0f995c6aec2097a4b8b80fc43f9e1faedea994f0
Teo Sarca <teo.sarca@metasfresh.com>
2017-08-31 17:09:10 +0300

    refactor and introduce HUMoveToDirectWarehouseService
    https://github.com/metasfresh/metasfresh/issues/2145
    (cherry picked from commit 90da8f03ea49680107e62096f32e8c349e8e1d3d)
    
    solved Conflicts:
    	src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_HU_MoveToDirectWarehouse.java
    	src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_HU_MoveToDirectWarehouse_Mass.java

---
+++ src/main/java/de/metas/ui/web/handlingunits/process/WEBUI_M_HU_MoveToDirectWarehouse.java
@@ -0,0 +1,85 @@
+package de.metas.ui.web.handlingunits.process;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.common.collect.Iterators;
+
+import de.metas.handlingunits.model.I_M_HU;
+import de.metas.process.IProcessPrecondition;
+import de.metas.process.ProcessPreconditionsResolution;
+import de.metas.process.RunOutOfTrx;
+import de.metas.ui.web.handlingunits.HUEditorRow;
+import de.metas.ui.web.window.datatypes.DocumentId;
+import de.metas.ui.web.window.datatypes.DocumentIdsSelection;
+import de.metas.ui.web.window.model.DocumentCollection;
+
+/*
+ * #%L
+ * metasfresh-webui-api
+ * %%
+ * Copyright (C) 2017 metas GmbH
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+/**
+ * HU Editor: Move selected HU to direct warehouse (aka Materialentnahme)
+ *
+ * @author metas-dev <dev@metasfresh.com>
+ *
+ */
+public class WEBUI_M_HU_MoveToDirectWarehouse extends HUEditorProcessTemplate implements IProcessPrecondition
+{
+	@Autowired
+	private DocumentCollection documentsCollection;
+
+	@Override
+	protected ProcessPreconditionsResolution checkPreconditionsApplicable()
+	{
+		final DocumentIdsSelection selectedRowIds = getSelectedDocumentIds();
+		if (!selectedRowIds.isSingleDocumentId())
+		{
+			return ProcessPreconditionsResolution.rejectBecauseNotSingleSelection();
+		}
+
+		final DocumentId rowId = selectedRowIds.getSingleDocumentId();
+		final HUEditorRow huRow = getView().getById(rowId);
+		if (!huRow.isTopLevel())
+		{
+			return ProcessPreconditionsResolution.rejectWithInternalReason("not a top level HU");
+		}
+
+		return ProcessPreconditionsResolution.accept();
+	}
+
+	@Override
+	@RunOutOfTrx
+	protected String doIt()
+	{
+		final I_M_HU hu = getRecord(I_M_HU.class);
+		
+		HUMoveToDirectWarehouseService.newInstance()
+				.setDocumentsCollection(documentsCollection)
+				.setHUView(getView())
+				// .setMovementDate(movementDate) // now
+				// .setDescription(description) // none
+				.setFailOnFirstError(true)
+				.setLoggable(this)
+				.move(Iterators.singletonIterator(hu));
+
+		return MSG_OK;
+	}
+}
