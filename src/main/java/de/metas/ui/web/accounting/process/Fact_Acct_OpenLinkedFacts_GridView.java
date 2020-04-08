7afff00ce74ae465dc3cb04a76a583ae56d58acb
metas-rc <ruxandra.craciunescu@metasfresh.com>
2020-04-08 12:22:08 +0300

    #6432 helper class for common logic in the 2 fact acct processes
    https://github.com/metasfresh/metasfresh/issues/6432

--- src/main/java/de/metas/ui/web/accounting/process/Fact_Acct_OpenLinkedFacts_GridView.java
+++ src/main/java/de/metas/ui/web/accounting/process/Fact_Acct_OpenLinkedFacts_GridView.java
@@ -1,28 +1,16 @@
 package de.metas.ui.web.accounting.process;
 
-import org.adempiere.util.lang.impl.TableRecordReference;
-import org.compiere.model.I_Fact_Acct;
-
-import com.google.common.collect.ImmutableList;
-
-import static org.adempiere.model.InterfaceWrapperHelper.getTableId;
-
-import de.metas.acct.api.IFactAcctDAO;
-import de.metas.document.engine.IDocument;
-import de.metas.document.engine.IDocumentBL;
 import de.metas.process.IProcessPrecondition;
-import de.metas.process.ProcessExecutionResult.RecordsToOpen;
+import de.metas.process.ProcessExecutionResult;
 import de.metas.process.ProcessPreconditionsResolution;
 import de.metas.ui.web.process.adprocess.ViewBasedProcessTemplate;
 import de.metas.ui.web.view.IViewRow;
 import de.metas.ui.web.window.datatypes.DocumentId;
-import de.metas.util.Services;
 
 public class Fact_Acct_OpenLinkedFacts_GridView extends ViewBasedProcessTemplate implements IProcessPrecondition
 {
-	final IFactAcctDAO factAcctDAO = Services.get(IFactAcctDAO.class);
-	final IDocumentBL documentBL = Services.get(IDocumentBL.class);
 
+	
 	@Override
 	public ProcessPreconditionsResolution checkPreconditionsApplicable()
 	{
@@ -45,25 +33,9 @@ public class Fact_Acct_OpenLinkedFacts_GridView extends ViewBasedProcessTemplate
 
 		final DocumentId factAcctId = singleSelectedRow.getId();
 
-		final I_Fact_Acct factAcct = factAcctDAO.getById(factAcctId.toInt());
+		final ProcessExecutionResult result = getResult();
 
-		final int factAcctTableId = getTableId(I_Fact_Acct.class);
-
-		final TableRecordReference documentReference = TableRecordReference.of(factAcct.getAD_Table_ID(), factAcct.getRecord_ID());
-
-		final IDocument document = documentBL.getDocument(documentReference);
-
-		ImmutableList<TableRecordReference> linkedFactAccts = factAcctDAO.retrieveQueryForDocument(document)
-				.create()
-				.listIds()
-				.stream()
-				.map(recordId -> TableRecordReference.of(factAcctTableId, recordId))
-				.collect(ImmutableList.toImmutableList());
-
-		getResult().setRecordToOpen(RecordsToOpen.builder()
-				.records(linkedFactAccts)
-				.automaticallySetReferencingDocumentPaths(false)
-				.build());
+		new Fact_Acct_OpenLinkedFacts_ProcessHelper().openLinkedFactAccounts(factAcctId.toInt(), result);
 
 		return MSG_OK;
 	}
--- src/main/java/de/metas/ui/web/accounting/process/Fact_Acct_OpenLinkedFacts_SingleView.java
+++ src/main/java/de/metas/ui/web/accounting/process/Fact_Acct_OpenLinkedFacts_SingleView.java
@@ -1,28 +1,14 @@
 package de.metas.ui.web.accounting.process;
 
-import org.adempiere.util.lang.impl.TableRecordReference;
-import org.compiere.model.I_Fact_Acct;
-
-import com.google.common.collect.ImmutableList;
-
-import static org.adempiere.model.InterfaceWrapperHelper.getTableId;
-
-import de.metas.acct.api.IFactAcctDAO;
-import de.metas.document.engine.IDocument;
-import de.metas.document.engine.IDocumentBL;
 import de.metas.process.IProcessPrecondition;
 import de.metas.process.IProcessPreconditionsContext;
 import de.metas.process.JavaProcess;
-import de.metas.process.ProcessExecutionResult.RecordsToOpen;
+import de.metas.process.ProcessExecutionResult;
 import de.metas.process.ProcessPreconditionsResolution;
-import de.metas.util.Services;
 import lombok.NonNull;
 
 public class Fact_Acct_OpenLinkedFacts_SingleView extends JavaProcess implements IProcessPrecondition
 {
-
-	final IFactAcctDAO factAcctDAO = Services.get(IFactAcctDAO.class);
-	final IDocumentBL documentBL = Services.get(IDocumentBL.class);
 
 	@Override
 	public ProcessPreconditionsResolution checkPreconditionsApplicable(@NonNull final IProcessPreconditionsContext context)
@@ -38,26 +24,9 @@ public class Fact_Acct_OpenLinkedFacts_SingleView extends JavaProcess implements
 	@Override
 	protected String doIt() throws Exception
 	{
-		final I_Fact_Acct factAcct = factAcctDAO.getById(getRecord_ID());
+		final ProcessExecutionResult result = getResult();
+		new Fact_Acct_OpenLinkedFacts_ProcessHelper().openLinkedFactAccounts(getRecord_ID(), result);
 
-		final int factAcctTableId = getTableId(I_Fact_Acct.class);
-		
-		final TableRecordReference documentReference = TableRecordReference.of(factAcct.getAD_Table_ID(), factAcct.getRecord_ID());
-		
-		final IDocument document = documentBL.getDocument(documentReference);
-		
-		final  ImmutableList<TableRecordReference> linkedFactAccts = factAcctDAO.retrieveQueryForDocument(document)
-				.create()
-				.listIds()
-				.stream()
-				.map(recordId -> TableRecordReference.of(factAcctTableId, recordId))
-				.collect(ImmutableList.toImmutableList());
-
-		getResult().setRecordToOpen(RecordsToOpen.builder()
-				.records(linkedFactAccts)
-				.automaticallySetReferencingDocumentPaths(false)
-				.build());
-		
 		return MSG_OK;
 	}
 
