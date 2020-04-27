e63b04ef75f02a7f0f4984e3725eef7226334090
TheBestPessimist <cristian@tbp.land>
2020-04-27 11:03:02 +0300

    Fix wrong import
    https://github.com/metasfresh/metasfresh/issues/6504

--- src/main/java/de/metas/ui/web/comments/json/JSONCommentCreateRequest.java
+++ src/main/java/de/metas/ui/web/comments/json/JSONCommentCreateRequest.java
@@ -25,18 +25,18 @@ package de.metas.ui.web.comments.json;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sun.istack.internal.NotNull;
+import lombok.NonNull;
 import lombok.Value;
 
 @Value
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE)
 public class JSONCommentCreateRequest
 {
-	@NotNull
+	@NonNull
 	String text;
 
 	@JsonCreator
-	public JSONCommentCreateRequest(@NotNull @JsonProperty("text") final String text)
+	public JSONCommentCreateRequest(@NonNull @JsonProperty("text") final String text)
 	{
 		this.text = text;
 	}
